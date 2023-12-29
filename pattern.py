89e29821ce0f31495ccdc1e4a7c6f32c5fdbb0b3
SamirSSR <rsamiran717@gmail.com>
2023-12-29 08:48:35 +0530

    second commit patterns specifically

--- pattern.py
+++ pattern.py
@@ -137,8 +137,85 @@ def hollw_diamond(h):
         else:
             count = count - 1
 
+def factorial(n):
+    if(n<=1):
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def pascaltriangle(h):
+    count = 0
+    coef = 1
+    while(count<h):
+        tem = 0
+        print(" "*(h-count), end=" ")
+        while(tem<=count):
+            coef = factorial(count)/(factorial(count-tem)*factorial(tem))
+            print(str(int(coef)), end=" ")
+            tem = tem + 1
+        print("")
+        count = count + 1
+
+def diamondwithnumbers(h):
+    count = 1
+    flag = True
+    while(count<=(h-1) and count >=1):
+        st = ""
+        if (count==1):
+            st = str(count)
+        else:
+            tem = count
+            flag_tem = False
+            while(tem>=1 and tem<=count):
+                if(tem==1):
+                    flag_tem=True
+                if(flag_tem):
+                    st = st + str(tem)
+                    tem = tem + 1
+                else:
+                    st = st + str(tem)
+                    tem = tem - 1                
+        if(count==(h-1)):
+            flag = False
+        if(flag):
+            print(" "*(h-count)+st)
+            count = count + 1
+        else:
+            print(" "*(h-count)+st)
+            count = count - 1 
+
+def hollw_diamond_withinrectangle(h):
+    count = 0
+    flag = True
+    while(count<=h and count>=0):
+        if(count==h):
+            flag=False
+            count = count-1
+        if(flag):
+            print("*"*(h-count)+" "*(2*count)+"*"*(h-count))
+            count= count+1
+        else:
+            print("*"*(h-count)+" "*(2*count)+"*"*(h-count))
+            count = count-1
+
+def butterfly(h):
+    count = 1
+    flag = True 
+    while(count<=h and count>=1):
+        if(count == h):
+            flag =  False
+        if(flag):
+            print("*"*(count)+" "*2*(h-count)+"*"*(count))
+            count = count + 1
+        else:
+            print("*"*(count)+" "*2*(h-count)+"*"*(count))
+            count = count - 1
+
 if __name__ == "__main__":
-    
+    butterfly(5)
+    # hollw_diamond_withinrectangle(5)
+    # diamondwithnumbers(5)
+    # pascaltriangle(5)
     # hollw_diamond(5)
     # hollow_triangle_reverse(5)
     # hollow_triangle(5)
